cc236ad39aec1c529b265f5322c2d1cc032ab5c0
kinjal <Promact2013>
2017-04-13 14:48:52 +0530

    Implmentation CRUD API for Modules and Scenario

---
+++ src/Promact.TestCaseManagement.Repository/ModuleRepository/IModuleRepository.cs
@@ -0,0 +1,49 @@
+﻿using Promact.TestCaseManagement.DomainModel.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Promact.TestCaseManagement.Repository.ModuleRepository
+{
+    public interface IModuleRepository
+    {
+        /// <summary>
+        /// Method to get all modules
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Module>> GetModulesAsync(int projectId);
+
+        /// <summary>
+        /// Method to add module to the database
+        /// </summary>
+        /// <param name="module">Module object</param>
+        /// <returns></returns>
+        Task<Module> AddModuleAsync(Module module);
+
+        /// <summary>
+        /// Method to update module to the database
+        /// </summary>
+        /// <param name="module">Module object</param>
+        /// <returns></returns>
+        Task<Module> UpdateModuleAsync(Module module);
+
+        /// <summary>
+        /// Method to delete module from the database
+        /// </summary>
+        /// <param name="module">Module object</param>
+        Task DeleteModuleAsync(Module module);
+
+        /// <summary>
+        /// Method to retrieve single module from database
+        /// </summary>
+        /// <param name="moduleId">Id of the module</param>
+        /// <returns></returns>
+        Task<Module> GetModuleAsync(int projectId,int moduleId);
+
+        /// <summary>
+        /// Method to check module is exist or not
+        /// </summary>
+        /// <param name="moduleId"></param>
+        /// <returns></returns>
+        Task<bool> IsModuleExistAsync(int moduleId);
+    }
+}
---
+++ src/Promact.TestCaseManagement.Repository/ModuleRepository/ModuleRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Promact.TestCaseManagement.DomainModel.DataContext;
+using Promact.TestCaseManagement.DomainModel.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promact.TestCaseManagement.Repository.ModuleRepository
+{
+    public class ModuleRepository : IModuleRepository
+    {
+        #region Private Member(s)
+
+        readonly TestCaseManagementDbContext _dbContext;
+
+        #endregion
+
+        #region Constructor
+
+        public ModuleRepository(TestCaseManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+        #region Public Method(s)
+
+   
+        public async Task<List<Module>> GetModulesAsync(int projectId)
+        {
+            return await _dbContext.Module.Where(x => x.ProjectId == projectId).ToListAsync();
+        }
+              
+        public async Task<Module> AddModuleAsync(Module module)
+        {
+            await _dbContext.Module.AddAsync(module);
+            await _dbContext.SaveChangesAsync();
+            return module;
+        }
+              
+        public async Task<Module> UpdateModuleAsync(Module module)
+        {
+            _dbContext.Module.Update(module);
+            await _dbContext.SaveChangesAsync();
+            return module;
+        }
+               
+        public async Task DeleteModuleAsync(Module module)
+        {
+            _dbContext.Module.Remove(module);
+            await _dbContext.SaveChangesAsync();
+        }
+               
+        public async Task<Module> GetModuleAsync(int projectId,int moduleId)
+        {
+            return await _dbContext.Module.FirstOrDefaultAsync(x=>x.Id == moduleId && x.ProjectId == projectId);
+        }
+
+        public async Task<bool> IsModuleExistAsync(int moduleId)
+        {
+            return await _dbContext.Module.AnyAsync(x => x.Id == moduleId);
+        }
+
+        #endregion
+    }
+}
