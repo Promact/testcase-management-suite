b08835608993f528659eac3b38e03c4ce2ee1f93
krupa <krupa@promactinfo.com>
2017-04-20 10:27:03 +0530

    Fixes: #21 Design of Modules Pages-Listing and Add Module,Testcase grid listing Completed and working on Create new testcase design

---
+++ src/Promact.TestCaseManagement/wwwroot/app/modules/modules.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core'
+import { modulesRouting } from './modules-route';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'modules.html'
+})
+
+export class ModulesComponent { }
