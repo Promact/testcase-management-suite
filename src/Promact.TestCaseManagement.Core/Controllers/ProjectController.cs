c572fc956b252c9c2fb5f2d2a2fe90775c2d23b1
kunal <kunal@PR1C4R3Y2016>
2017-04-24 14:04:28 +0530

    string constant related changes in project controller

--- src/Promact.TestCaseManagement.Core/Controllers/ProjectController.cs
+++ src/Promact.TestCaseManagement.Core/Controllers/ProjectController.cs
@@ -8,20 +8,23 @@ using System.Threading.Tasks;
 namespace Promact.TestCaseManagement.Core.Controllers
 {
     [Authorize]
-    [Route(StringConstants.ProjectBaseUrl)]
+    [Route(BaseUrl)]
     public class ProjectController : Controller
     {
         #region "Private Member(s)"
 
         readonly IProjectRepository _iProjectRepository;
+        internal const string BaseUrl = "api/project";
+        IStringConstant _iStringConstant;
 
         #endregion
 
         #region "Constructor"
 
-        public ProjectController(IProjectRepository iProjectRepository)
+        public ProjectController(IProjectRepository iProjectRepository, IStringConstant iStringConstant)
         {
             _iProjectRepository = iProjectRepository;
+            _iStringConstant = iStringConstant;
         }
 
         #endregion
@@ -37,7 +40,7 @@ namespace Promact.TestCaseManagement.Core.Controllers
         {
             if (User.Identity.IsAuthenticated)
             {
-                return Ok(await _iProjectRepository.GetProjectsAsync(User.Claims.Single(x => x.Type.Equals(StringConstants.Sub)).Value));
+                return Ok(await _iProjectRepository.GetProjectsAsync(User.Claims.Single(x => x.Type.Equals(_iStringConstant.Sub)).Value));
             }
             return Unauthorized();
         }
