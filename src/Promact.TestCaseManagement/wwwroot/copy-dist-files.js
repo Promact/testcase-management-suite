f6f927b2cef18f235e8e839b5680ff1a573d5fc6
kunal <kunal@PR1C4R3Y2016>
2017-05-01 16:55:31 +0530

    file path set in node script

--- src/Promact.TestCaseManagement/wwwroot/copy-dist-files.js
+++ src/Promact.TestCaseManagement/wwwroot/copy-dist-files.js
@@ -4,10 +4,10 @@ var resources = [
     'node_modules/zone.js/dist/zone.min.js',
     'wwwroot/css/style.css',
     'node_modules/bootstrap/dist/css/bootstrap.css',
-    'node_modules/@angular/material/core/theming/prebuilt/indigo-pink.css'
+    'node_modules/@angular/material/prebuilt-themes/indigo-pink.css'
 ];
 resources.map(function (f) {
     var path = f.split('/');
-    var t = 'aot/' + path[path.length - 1];
+    var t = 'wwwroot/aot/' + path[path.length - 1];
     fs.createReadStream(f).pipe(fs.createWriteStream(t));
 });
