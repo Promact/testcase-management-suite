d5106f29683a55c5048869f09ee86f8035b5e32b
kunal <kunal@PR1C4R3Y2016>
2017-05-01 14:53:34 +0530

    aot script file related changes applied

--- src/Promact.TestCaseManagement/wwwroot/copy-dist-files.js
+++ src/Promact.TestCaseManagement/wwwroot/copy-dist-files.js
@@ -1,10 +1,10 @@
 ﻿var fs = require('fs');
 var resources = [
-    '../node_modules/core-js/client/shim.min.js',
-    '../node_modules/zone.js/dist/zone.min.js',
-    'css/style.css',
-    '../node_modules/bootstrap/dist/css/bootstrap.css',
-    '../node_modules/@angular/material/core/theming/prebuilt/indigo-pink.css'
+    'node_modules/core-js/client/shim.min.js',
+    'node_modules/zone.js/dist/zone.min.js',
+    'wwwroot/css/style.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/@angular/material/core/theming/prebuilt/indigo-pink.css'
 ];
 resources.map(function (f) {
     var path = f.split('/');
