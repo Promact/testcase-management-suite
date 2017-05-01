307ffe6a2e03680b64cfe59b033860e10b50c483
kunal <kunal@PR1C4R3Y2016>
2017-05-01 12:42:16 +0530

    fixes #31 AOT related changes applied

---
+++ src/Promact.TestCaseManagement/wwwroot/copy-dist-files.js
@@ -0,0 +1,13 @@
+﻿var fs = require('fs');
+var resources = [
+    'node_modules/core-js/client/shim.min.js',
+    'node_modules/zone.js/dist/zone.min.js',
+    'src/styles.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/@@angular/material/prebuilt-themes/indigo-pink.css'
+];
+resources.map(function (f) {
+    var path = f.split('/');
+    var t = 'aot/' + path[path.length - 1];
+    fs.createReadStream(f).pipe(fs.createWriteStream(t));
+});
---
+++ src/Promact.TestCaseManagement/wwwroot/rollup-config.js
@@ -0,0 +1,26 @@
+﻿import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry: 'wwwroot/app/main.aot.js',
+    dest: 'wwwroot/app/build.js',
+    sourceMap: false,
+    format: 'iife',
+    onwarn: function (warning) {
+        // Skip certain warnings
+
+        // should intercept ... but doesn't in some rollup versions
+        if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+
+        // console.warn everything else
+        console.warn(warning.message);
+    },
+    plugins: [
+        nodeResolve({ jsnext: true, module: true }),
+        commonjs({
+            include: ['node_modules/**', 'node_modules/rxjs/**']
+        }),
+        uglify()
+    ]
+};
