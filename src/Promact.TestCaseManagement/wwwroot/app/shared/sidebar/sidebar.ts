6c42231e3acd647047997e7d60e5d64bf005359e
krupa <krupa@promactinfo.com>
2017-05-05 17:09:47 +0530

    Issue:#34 -Design of dashbaord and basic layout

--- src/Promact.TestCaseManagement/wwwroot/app/shared/sidebar/sidebar.ts
+++ src/Promact.TestCaseManagement/wwwroot/app/shared/sidebar/sidebar.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
 
 @Component({
-	moduleId: module.id,
-	selector: 'sidebar-cmp',
-	templateUrl: 'sidebar.html'
+  moduleId: module.id,
+  selector: 'sidebar-cmp',
+  templateUrl: 'sidebar.html'
 })
 
 export class SidebarComponent {
-	isActive = false;
-	showMenu: string = '';
-	eventCalled() {
-		this.isActive = !this.isActive;
-	}
-	addExpandClass(element: any) {
-		if (element === this.showMenu) {
-			this.showMenu = '0';
-		} else {
-			this.showMenu = element;
-		}
-	}
+  subOptionShow: boolean = true;
+
+  constructor(private router: Router) {
+    router.events.filter(event => event instanceof RoutesRecognized).subscribe((val: RoutesRecognized) => {
+      this.subOptionShow = val.urlAfterRedirects.includes("projects");
+    });
+  }
 }
