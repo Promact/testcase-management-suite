70d6cf3f53aecc9f87377d568682acf2d58f05f7
jay <jay@PR2C3R1Y2016>
2017-05-11 15:57:53 +0530

    Design of Module, Scenario and Testcase ( Wireframes ).

---
+++ src/Promact.TestCaseManagement/wwwroot/app/scenarios/scenarios.module.ts
@@ -0,0 +1,27 @@
+﻿import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScenarioListComponent } from './scenario-list.component'
+import { ScenarioComponent } from './scenario.component';
+import { scenariosRouting } from './scenario-route';
+import { SharedModule } from '../shared/shared.module';
+import { Md2Module } from 'md2';
+import { ScenarioTestcaseAssignComponent } from './assigntestcases/scenario-testcase-assignment.component';
+
+@NgModule({
+    imports: [
+        SharedModule,
+        scenariosRouting,
+        BrowserAnimationsModule,
+        Md2Module.forRoot()
+    ],
+    declarations: [
+        ScenarioTestcaseAssignComponent,
+        ScenarioListComponent,
+        ScenarioComponent
+    ],
+    providers: [
+
+    ]
+})
+
+export class ScenariosModule { }
